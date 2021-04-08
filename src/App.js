c0a67da9ec70a5dce74c9c9bd570a5c2e34496b2
Ezequiel Izquierdo <ezequielizquierdo@hotmail.com>
2021-04-08 20:11:33 -0300

    "Carga segunda entrega"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import AppBar from './src/AppBar'
+import React from 'react'
+import AppBar from './components/AppBar'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <AppBar>
-
       </AppBar>
+      <h1>atHome coming soon</h1>
     </div>
   );
 }
